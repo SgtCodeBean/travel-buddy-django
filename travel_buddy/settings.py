094099794e63d96f2e9161bd8ee5ad3962095584
SgtCodeBean <bran5816@gmail.com>
2025-04-16 07:36:25 -0400

    Adjusted settings to pull from proper database

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -77,11 +77,22 @@ WSGI_APPLICATION = 'travel_buddy.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cs5774db",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
